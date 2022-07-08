9bcc2601ef30b041c660e6e1e99d2431bc3902ad
ablin <space6fic@gmail.com>
2022-07-08 23:42:22 +0200

    Server now retrieving owner for /latest, adapted frontend to match that change

--- server/Services/NFTService.ts
+++ server/Services/NFTService.ts
@@ -1,75 +1,102 @@
 import { NFT } from "../Models/NFT";
 import { connectToWallet, getRandomNumber } from "../Utils";
-import { Interface } from 'ethers/lib/utils';
-import mergeImages from 'merge-images';
-import { Canvas, Image } from 'canvas';
+import { Interface } from "ethers/lib/utils";
+import mergeImages from "merge-images";
+import { Canvas, Image } from "canvas";
 import { logError, logInfo, logWarn } from "../Config/Logger";
-import { BACKGROUND_COMPONENTS, PLAYER_COMPONENTS, WEAPON_COMPONENTS } from "../Data/NFTComponents";
-import { Contract, ethers } from 'ethers';
+import {
+  BACKGROUND_COMPONENTS,
+  PLAYER_COMPONENTS,
+  WEAPON_COMPONENTS,
+} from "../Data/NFTComponents";
+import { Contract, ethers } from "ethers";
 import { WALLET_PRIVATE_KEY } from "../Config/Config";
-import db from './db/DBService';
-import { DEFENDER_ABI, DEFENDER_CONTRACT_ADDRESS, DungeonDefendersContract, DungeonLootContract } from 'dungeon-defenders-contracts';
+import db from "./db/DBService";
+import {
+  DEFENDER_ABI,
+  DEFENDER_CONTRACT_ADDRESS,
+  DungeonDefendersContract,
+  DungeonLootContract,
+} from "dungeon-defenders-contracts";
 
-export async function getNFT(tokenId: string, exisitingContract?: DungeonDefendersContract | DungeonLootContract) : Promise<NFT | undefined> {
-    try {
-        let contract = exisitingContract;
-        if (!contract) {
-            const connectResult = connectToDungeonDefenderContract('getNFT');
-            if (!connectResult) {
-                return undefined;
-            }
-        
-            const {contract: c} = connectResult;
-            contract = c;
-        }
-    
-        const tokenURIBase64 = await contract.tokenURI(tokenId);
-        const jsonBase64 = tokenURIBase64.split(',')[1];
-        const jsonStr = Buffer.from(jsonBase64, 'base64').toString('utf-8');
-        return JSON.parse(jsonStr) as NFT;
-    } catch (error) {
-        logError(error as any, 'getNFT');
+export async function getNFT(
+  tokenId: string,
+  exisitingContract?: DungeonDefendersContract | DungeonLootContract
+): Promise<NFT | undefined> {
+  try {
+    let contract = exisitingContract;
+    if (!contract) {
+      const connectResult = connectToDungeonDefenderContract("getNFT");
+      if (!connectResult) {
+        return undefined;
+      }
+
+      const { contract: c } = connectResult;
+      contract = c;
     }
+
+    const tokenURIBase64 = await contract.tokenURI(tokenId);
+    const jsonBase64 = tokenURIBase64.split(",")[1];
+    const jsonStr = Buffer.from(jsonBase64, "base64").toString("utf-8");
+    return JSON.parse(jsonStr) as NFT;
+  } catch (error) {
+    logError(error as any, "getNFT");
+  }
 }
 
-export async function getNFTCollection(address: string) : Promise<NFT[]> {
-    const tokenIds = await db.getNFTCollection(address);
-    if (!tokenIds || tokenIds.length === 0) {
-        logWarn(`No NFTs for address=${address}`, 'getNFTCollection')
-        return [];
-    }
+export async function getNFTCollection(address: string): Promise<NFT[]> {
+  const tokenIds = await db.getNFTCollection(address);
+  if (!tokenIds || tokenIds.length === 0) {
+    logWarn(`No NFTs for address=${address}`, "getNFTCollection");
+    return [];
+  }
 
-    const connectResult = connectToDungeonDefenderContract('getNFTCollection');
-    if (!connectResult) {
-        return [];
-    }
-    const {contract} = connectResult;
+  const connectResult = connectToDungeonDefenderContract("getNFTCollection");
+  if (!connectResult) {
+    return [];
+  }
+  const { contract } = connectResult;
 
-    const ops : Promise<NFT | undefined>[] = tokenIds.map(t => getNFT(t, contract));
-    const responses = await Promise.all(ops);
-    const NFTs = responses.filter(r => !!r) as NFT[];
+  const ops: Promise<NFT | undefined>[] = tokenIds.map((t) =>
+    getNFT(t, contract)
+  );
+  const responses = await Promise.all(ops);
+  const NFTs = responses.filter((r) => !!r) as NFT[];
 
-    return NFTs;
+  return NFTs;
 }
 
-export async function getLatestNFTs(numOfNFTs: number) : Promise<NFT[]> {
-    const nftToMint = await db.getNFTToMint();
+export async function getLatestNFTs(numOfNFTs: number): Promise<NFT[]> {
+  const nftToMint = await db.getNFTToMint();
 
-    const tokenIds = Object.keys(nftToMint)
-            .sort((a, b) => nftToMint[b] - nftToMint[a])
-            .splice(0, numOfNFTs);
-            
-    const connectResult = connectToDungeonDefenderContract('getLatestNFTs');
-    if (!connectResult) {
-        return [];
-    }
-    const {contract} = connectResult;
+  const tokenIds = Object.keys(nftToMint)
+    .sort((a, b) => nftToMint[b] - nftToMint[a])
+    .splice(0, numOfNFTs);
 
-    const ops : Promise<NFT | undefined>[] = tokenIds.map(t => getNFT(t, contract));
-    const responses = await Promise.all(ops);
-    const NFTs = responses.filter(r => !!r) as NFT[];
+  const connectResult = connectToDungeonDefenderContract("getLatestNFTs");
+  if (!connectResult) {
+    return [];
+  }
+  const { contract } = connectResult;
 
-    return NFTs;
+  const ops: Promise<NFT | undefined>[] = tokenIds.map((t) =>
+    getNFT(t, contract)
+  );
+  const responses = await Promise.all(ops);
+
+  const users: Promise<string | undefined>[] = tokenIds.map((t) =>
+    db.getNFTOwner(t)
+  );
+  const owners = await Promise.all(users);
+  const result = responses.map((item, index) => {
+    return {
+      ...item,
+      owner: owners[index],
+    };
+  });
+  const NFTs = result.filter((r) => !!r) as NFT[];
+
+  return NFTs;
 }
 
 /**
@@ -77,112 +104,128 @@ export async function getLatestNFTs(numOfNFTs: number) : Promise<NFT[]> {
  * @param tokenId The NFT to render
  * @returns Data URI for Rendered NFT
  */
-export async function renderNFT(tokenId: string) : Promise<Buffer> {
-    // TODO make accurate
-    const background = BACKGROUND_COMPONENTS[getRandomNumber(0, 4)];
-    const player = PLAYER_COMPONENTS[getRandomNumber(0, 6)];
-    const weapon = WEAPON_COMPONENTS[getRandomNumber(0, 4)];
+export async function renderNFT(tokenId: string): Promise<Buffer> {
+  // TODO make accurate
+  const background = BACKGROUND_COMPONENTS[getRandomNumber(0, 4)];
+  const player = PLAYER_COMPONENTS[getRandomNumber(0, 6)];
+  const weapon = WEAPON_COMPONENTS[getRandomNumber(0, 4)];
 
-    logInfo(`background=${JSON.stringify(background)}`, 'generateNFT');
-    logInfo(`player=${JSON.stringify(player)}`, 'generateNFT');
-    logInfo(`weapon=${JSON.stringify(weapon)}`, 'generateNFT');
+  logInfo(`background=${JSON.stringify(background)}`, "generateNFT");
+  logInfo(`player=${JSON.stringify(player)}`, "generateNFT");
+  logInfo(`weapon=${JSON.stringify(weapon)}`, "generateNFT");
 
-    const mergedImage = await mergeImages([
-        background.imgPath,
-        {
-            src: `Assets/Floor/${getRandomNumber(1, 9)}.png`,
-            x: 0,
-            y: 160
-        },
-        {
-            src: `Assets/Floor/${getRandomNumber(1, 9)}.png`,
-            x: 160,
-            y: 160
-        },
-        {
-            src: `Assets/Floor/${getRandomNumber(1, 9)}.png`,
-            x: 320,
-            y: 160
-        },
-        {
-            src: `Assets/Floor/${getRandomNumber(1, 9)}.png`,
-            x: 0,
-            y: 320
-        },
-        {
-            src: `Assets/Floor/${getRandomNumber(1, 9)}.png`,
-            x: 160,
-            y: 320
-        },
-        {
-            src: `Assets/Floor/${getRandomNumber(1, 9)}.png`,
-            x: 320,
-            y: 320
-        },
-        player.imgPath,
-        weapon.imgPath], {
-        Canvas,
-        Image
-    });
-    const mergedImageBuffer = Buffer.from(mergedImage.substring(mergedImage.indexOf(',')));
+  const mergedImage = await mergeImages(
+    [
+      background.imgPath,
+      {
+        src: `Assets/Floor/${getRandomNumber(1, 9)}.png`,
+        x: 0,
+        y: 160,
+      },
+      {
+        src: `Assets/Floor/${getRandomNumber(1, 9)}.png`,
+        x: 160,
+        y: 160,
+      },
+      {
+        src: `Assets/Floor/${getRandomNumber(1, 9)}.png`,
+        x: 320,
+        y: 160,
+      },
+      {
+        src: `Assets/Floor/${getRandomNumber(1, 9)}.png`,
+        x: 0,
+        y: 320,
+      },
+      {
+        src: `Assets/Floor/${getRandomNumber(1, 9)}.png`,
+        x: 160,
+        y: 320,
+      },
+      {
+        src: `Assets/Floor/${getRandomNumber(1, 9)}.png`,
+        x: 320,
+        y: 320,
+      },
+      player.imgPath,
+      weapon.imgPath,
+    ],
+    {
+      Canvas,
+      Image,
+    }
+  );
+  const mergedImageBuffer = Buffer.from(
+    mergedImage.substring(mergedImage.indexOf(","))
+  );
 
-    return mergedImageBuffer;
+  return mergedImageBuffer;
 }
 
 export function connectToDungeonDefenderContract(funcName: string) {
-    if (!WALLET_PRIVATE_KEY) {
-        logError('Don\'t have wallet secret key setup', funcName);
-        return;
-    }
-    if (!DEFENDER_CONTRACT_ADDRESS) {
-        logError('Don\'t have nft contract address setup', funcName);
-        return;
-    }
+  if (!WALLET_PRIVATE_KEY) {
+    logError("Don't have wallet secret key setup", funcName);
+    return;
+  }
+  if (!DEFENDER_CONTRACT_ADDRESS) {
+    logError("Don't have nft contract address setup", funcName);
+    return;
+  }
 
-    const [provider, wallet, signer] = connectToWallet() ?? [];
-    if (!provider) {
-        logError('No provider', funcName);
-        return;
-    }
+  const [provider, wallet, signer] = connectToWallet() ?? [];
+  if (!provider) {
+    logError("No provider", funcName);
+    return;
+  }
 
-    const contract: DungeonDefendersContract = new Contract(
-        DEFENDER_CONTRACT_ADDRESS,
-        DEFENDER_ABI,
-        signer
-    ) as DungeonDefendersContract;
+  const contract: DungeonDefendersContract = new Contract(
+    DEFENDER_CONTRACT_ADDRESS,
+    DEFENDER_ABI,
+    signer
+  ) as DungeonDefendersContract;
 
-    return { provider, wallet, signer, contract}
+  return { provider, wallet, signer, contract };
 }
 
-async function handleTransferEvent(tokenId: number, newOwner: string, blockNumber?: number) {
-    const tokenIdStr = tokenId.toString();
-    if (blockNumber) {
-        await db.updateNFTMintTime(tokenIdStr, blockNumber);
-    }
-    await db.updateNFTOwner(tokenIdStr, newOwner);
+async function handleTransferEvent(
+  tokenId: number,
+  newOwner: string,
+  blockNumber?: number
+) {
+  const tokenIdStr = tokenId.toString();
+  if (blockNumber) {
+    await db.updateNFTMintTime(tokenIdStr, blockNumber);
+  }
+  await db.updateNFTOwner(tokenIdStr, newOwner);
 }
 export function registerEventListeners() {
-    const connectResult = connectToDungeonDefenderContract('registerEventListeners');
-    if (!connectResult) {
-        return;
-    }
+  const connectResult = connectToDungeonDefenderContract(
+    "registerEventListeners"
+  );
+  if (!connectResult) {
+    return;
+  }
 
-    const {provider, contract: tokenContract} = connectResult;
-    const iface = new Interface(DEFENDER_ABI);
+  const { provider, contract: tokenContract } = connectResult;
+  const iface = new Interface(DEFENDER_ABI);
 
-    logInfo('Connecting to transfer events', 'registerEventListeners');
-    const transferFilter = tokenContract.filters.Transfer();
-    provider.on(transferFilter, async (log) => {
-        const parsedLog = iface.parseLog(log);
-        
-        const fromStr = parsedLog.args.from;
-        const toStr = parsedLog.args.to;
-        const tokenId = parsedLog.args.tokenId.toString();
-        logInfo(
-          `Transfered tokenId=${tokenId} from=${fromStr} to=${toStr}`,
-          "registerEventListeners"
-        );
-    
-        await handleTransferEvent(tokenId, toStr, ethers.constants.AddressZero === fromStr ? log.blockNumber : undefined);
-    });
+  logInfo("Connecting to transfer events", "registerEventListeners");
+  const transferFilter = tokenContract.filters.Transfer();
+  provider.on(transferFilter, async (log) => {
+    const parsedLog = iface.parseLog(log);
+
+    const fromStr = parsedLog.args.from;
+    const toStr = parsedLog.args.to;
+    const tokenId = parsedLog.args.tokenId.toString();
+    logInfo(
+      `Transfered tokenId=${tokenId} from=${fromStr} to=${toStr}`,
+      "registerEventListeners"
+    );
+
+    await handleTransferEvent(
+      tokenId,
+      toStr,
+      ethers.constants.AddressZero === fromStr ? log.blockNumber : undefined
+    );
+  });
 }
